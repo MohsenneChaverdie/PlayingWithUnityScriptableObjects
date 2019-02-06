77e2c721c5ad0c4e09fd94c87d9bf54b960c0c36
MohsenneChaverdie <mohsennechaverdie@gmail.com>
2019-02-06 08:32:19 +0100

    Initial commit.

---
+++ ScriptableObjectTestingUnityProject/Assets/Scripts/SpawnManagerScriptableObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+
+/// <inheritdoc />
+/// <summary>
+/// this class is a ScriptableObject. You can set its values in Editor and not in Runtime. It is very good for
+/// Editor scripting and usage of plugins.
+/// </summary>
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1)]
+public class SpawnManagerScriptableObject : ScriptableObject
+{
+    public string prefabName;
+    public int numberOfPrefabsToCreate;
+    public Vector3[] spawnPoints;
+}
---
+++ ScriptableObjectTestingUnityProject/Assets/Scripts/Spawner.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    /// <summary>
+    /// the prefab in this example we want to spawn
+    /// </summary>
+    public GameObject entityToSpawn;
+    
+    /// <summary>
+    /// reference to our scriptableObject, so we can read its public values.
+    /// </summary>
+    public SpawnManagerScriptableObject spawnManagerValues;
+    
+    /// <summary>
+    /// to have an incremental name for each instantiated prefab
+    /// </summary>
+    private int _instanceNumber = 1;
+
+    private void Start()
+    {
+        SpawnEntities();
+    }
+
+    /// <summary>
+    /// the method is responsible for based on scriptableObject values spawn a sample prefab.
+    /// </summary>
+    private void SpawnEntities()
+    {
+        var currentSpawnPointIndex = 0;
+
+        for (var i = 0; i < spawnManagerValues.numberOfPrefabsToCreate; i++)
+        {
+            var currentEntity = Instantiate(entityToSpawn,
+                spawnManagerValues.spawnPoints[currentSpawnPointIndex], Quaternion.identity);
+
+            currentEntity.name = spawnManagerValues.prefabName + _instanceNumber;
+
+            // to make sure we are using the corresponding vector position from our scriptableObject
+            currentSpawnPointIndex = (currentSpawnPointIndex + 1) % spawnManagerValues.spawnPoints.Length;
+
+            _instanceNumber++;
+        }
+    }
+}
